f8d19a8c81c403071e949868048f4f6ffee8a4a8
MJKCNEW <BansalRenu@JohnDeere.com>
2022-06-29 13:42:01 +0530

    Refactor the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import { Component } from "react/cjs/react.production.min";
 
+const CLIENT_ID = "e2b6fd6022f84996898f9e65f2fc5d16";
+const REDIRECT_URI = "http://localhost:1234/";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+const BASE_SPOTIFY_ADDRESS = "https://api.spotify.com/v1";
 
-function App() {
-  const CLIENT_ID = "e2b6fd6022f84996898f9e65f2fc5d16";
-  const REDIRECT_URI = "http://localhost:1234/";
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
-  const BASE_SPOTIFY_ADDRESS = 'https://api.spotify.com/v1';
-//   state = {};
+class App extends React.Component {
+  state = { token: "", searchKey: "", artist: "", tracks: [] };
 
-  const [token, setToken] = useState("");
-  const [searchKey, setSearchKey] = useState("");
-  const [artist, setArtist] = useState("");
-
-  useEffect(() => {
+  componentDidMount() {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
 
@@ -29,83 +26,98 @@ function App() {
       window.localStorage.setItem("token", token);
     }
 
-    setToken(token);
-  }, []);
+    this.setState({ token });
+  }
 
-  const logout = () => {
-    setToken("");
+  //   componentWillUnmount() {
+  //     this.logout();
+  //   }
+
+  logout = () => {
+    this.setState({ token: "" });
     window.localStorage.removeItem("token");
   };
 
-  const searchArtists = async (e) => {
-    console.log("search key  : ", searchKey);
+  searchArtists = () => {
     const requestOptions = {
-        method: 'GET',
-        headers: {  'Authorization': `Bearer ${token}` }
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.state.token}` },
     };
-    fetch(`${BASE_SPOTIFY_ADDRESS}/search?q=${searchKey}&type=artist&limit=1`, requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        if(data.artists.total >  0) {
-            console.log(data.artists.items[0]);
-            setArtist(data.artists.items[0]);
-            fetch(`${BASE_SPOTIFY_ADDRESS}/artists/${data.artists.items[0].id}/top-tracks?country=US`, requestOptions)
-            .then(response => response.json())
-            .then(json => console.log("tracks : ", json))
-            // .catch(err => alert(err.message))
+    fetch(
+      `${BASE_SPOTIFY_ADDRESS}/search?q=${this.state.searchKey}&type=artist&limit=1`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.artists.total > 0) {
+            const artist = data.artists.items[0];
+          this.setState({ artist });
+          fetch(
+            `${BASE_SPOTIFY_ADDRESS}/artists/${this.state.artist.id}/top-tracks?country=US`,
+            requestOptions
+          )
+            .then((response) => response.json())
+            .then((json) => this.setState({tracks: json.tracks}))
+          .catch(err => alert(err.message))
         }
-    });
+      })
+      .catch(err => alert(err.message));
   };
 
-  const renderArtists = () => {
-    // return artists.map((artist) => (
-    //   <div key={artist.id}>
-    //     {artist.images.length ? (
-    //       <img width={"100%"} src={artist.images[0].url} alt="" />
-    //     ) : (
-    //       <div>No Image</div>
-    //     )}
-    //     {artist.name}
-    //   </div>
-    // ));
+  //   renderArtists = () => {
+  //     // return artists.map((artist) => (
+  //     //   <div key={artist.id}>
+  //     //     {artist.images.length ? (
+  //     //       <img width={"100%"} src={artist.images[0].url} alt="" />
+  //     //     ) : (
+  //     //       <div>No Image</div>
+  //     //     )}
+  //     //     {artist.name}
+  //     //   </div>
+  //     // ));
+  //   };
+
+  updateArtistQuery = (event) => {
+    this.setState({ searchKey: event.target.value });
   };
 
-  const updateArtistQuery = event => {
-      setSearchKey(event.target.value);
+  handleKeyPress = (event) => {
+    if (event.key == "Enter") {
+      this.searchArtists();
+    }
+  };
+
+  render() {
+      console.log(this.state);
+    return (
+      <div>
+        <h1>Music</h1>
+        {!this.state.token ? (
+          <a
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+          >
+            Login to Spotify
+          </a>
+        ) : (
+          <button onClick={this.logout}>Logout</button>
+        )}
+
+        {this.state.token ? (
+          <form onSubmit={this.searchArtists}>
+            <input
+              type="text"
+              onChange={this.updateArtistQuery}
+              placeholder="Search for an Artist"
+              onKeyDown={this.handleKeyPress}
+            />
+            <button type={"submit"}>Search</button>
+          </form>
+        ) : (
+          <h2>Please login</h2>
+        )}
+      </div>
+    );
   }
-
-  const handleKeyPress = event => {
-      if(event.key == 'Enter') {
-          searchArtists();
-      }
-
-  } 
-
-
-  return (
-    <div>
-      <h1>Music</h1>
-      {!token ?
-                    <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                        to Spotify</a>
-                    : <button onClick={logout}>Logout</button>}
-
-                {token ?
-                    <form onSubmit={searchArtists}>
-                        <input 
-                            type="text"
-                            onChange={updateArtistQuery} 
-                            placeholder="Search for an Artist"
-                            onKeyDown={handleKeyPress}/>
-                        <button type={"submit"}>Search</button>
-                    </form>
-
-                    : <h2>Please login</h2>
-                }
-
-                {renderArtists()}
-    </div>
-  );
 }
 
 export default App;
