8a593c00975719c9ba50bbcefbb8bdb84d0970ed
MJKCNEW <BansalRenu@JohnDeere.com>
2022-06-29 15:16:34 +0530

    Split App into multiple components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
-import { Component } from "react/cjs/react.production.min";
+import Artist from "./Artist";
+import Search from "./Search";
+import Login from "./Login";
 
-const CLIENT_ID = "e2b6fd6022f84996898f9e65f2fc5d16";
-const REDIRECT_URI = "http://localhost:1234/";
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const RESPONSE_TYPE = "token";
 const BASE_SPOTIFY_ADDRESS = "https://api.spotify.com/v1";
 
 class App extends React.Component {
@@ -29,92 +26,45 @@ class App extends React.Component {
     this.setState({ token });
   }
 
-  //   componentWillUnmount() {
-  //     this.logout();
-  //   }
-
   logout = () => {
     this.setState({ token: "" });
     window.localStorage.removeItem("token");
   };
 
-  searchArtists = () => {
+  searchArtist = artistQuery => {
     const requestOptions = {
       method: "GET",
       headers: { Authorization: `Bearer ${this.state.token}` },
     };
     fetch(
-      `${BASE_SPOTIFY_ADDRESS}/search?q=${this.state.searchKey}&type=artist&limit=1`,
+      `${BASE_SPOTIFY_ADDRESS}/search?q=${artistQuery}&type=artist&limit=1`,
       requestOptions
     )
       .then((response) => response.json())
       .then((data) => {
         if (data.artists.total > 0) {
-            const artist = data.artists.items[0];
+          const artist = data.artists.items[0];
           this.setState({ artist });
           fetch(
             `${BASE_SPOTIFY_ADDRESS}/artists/${this.state.artist.id}/top-tracks?country=US`,
             requestOptions
           )
             .then((response) => response.json())
-            .then((json) => this.setState({tracks: json.tracks}))
-          .catch(err => alert(err.message))
+            .then((json) => this.setState({ tracks: json.tracks }))
+            .catch((err) => alert("Track : ", err.message));
         }
       })
-      .catch(err => alert(err.message));
-  };
-
-  //   renderArtists = () => {
-  //     // return artists.map((artist) => (
-  //     //   <div key={artist.id}>
-  //     //     {artist.images.length ? (
-  //     //       <img width={"100%"} src={artist.images[0].url} alt="" />
-  //     //     ) : (
-  //     //       <div>No Image</div>
-  //     //     )}
-  //     //     {artist.name}
-  //     //   </div>
-  //     // ));
-  //   };
-
-  updateArtistQuery = (event) => {
-    this.setState({ searchKey: event.target.value });
-  };
-
-  handleKeyPress = (event) => {
-    if (event.key == "Enter") {
-      this.searchArtists();
-    }
+      .catch((err) => alert(err.message));
   };
 
   render() {
-      console.log(this.state);
+    console.log(this.state);
     return (
       <div>
         <h1>Music</h1>
-        {!this.state.token ? (
-          <a
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-          >
-            Login to Spotify
-          </a>
-        ) : (
-          <button onClick={this.logout}>Logout</button>
-        )}
-
-        {this.state.token ? (
-          <form onSubmit={this.searchArtists}>
-            <input
-              type="text"
-              onChange={this.updateArtistQuery}
-              placeholder="Search for an Artist"
-              onKeyDown={this.handleKeyPress}
-            />
-            <button type={"submit"}>Search</button>
-          </form>
-        ) : (
-          <h2>Please login</h2>
-        )}
+        {!this.state.token ? (<Login />) : (<button onClick={this.logout}>Logout</button>)}
+        {this.state.token ? (<Search searchArtist={this.searchArtist} />) : (<h2>Please login</h2>)}
+        <Artist artist={this.state.artist} />
       </div>
     );
   }
